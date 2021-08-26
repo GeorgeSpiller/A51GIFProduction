b60d39e84d60bf46f7a6fc8bbe7638ce3e91ecf8
GeorgeSpiller <57860253+GeorgeSpiller@users.noreply.github.com>
2021-08-26 12:58:17 +0100

    Fix: text now covers canvas (ineffecient fix)

--- A51GIFProducer/assemble_teams.py
+++ A51GIFProducer/assemble_teams.py
@@ -17,9 +17,16 @@ def util_drawLine(startCoords, main, teamTextImage):
     teamText_start = startCoords
     teamText_end = [teamText_start[0] + teamTextImage.size[0], teamText_start[1] - teamTextImage.size[1]]
 
-    while teamText_start[0] < CANVAS_SIZE[0] and teamText_start[1] < CANVAS_SIZE[1]:
+    while teamText_start[0] < CANVAS_SIZE[0]:
+
         if teamText_end[0] < 0 and teamText_end[1] < 0:
-            continue
+            print(f"Cord caught off screen: {teamText_end}")
+            while teamText_end[0] < 0 and teamText_end[1] < 0:
+                teamText_start = teamText_end
+                teamText_end = [teamText_start[0] + teamTextImage.size[0], teamText_start[1] - teamTextImage.size[1]]
+                print(f"end cord off canvas; updated to: s: {teamText_start} e: {teamText_end}")
+        print(f"Drawing cord {teamText_start} to {teamText_end}")
+
         pasteCords = util_reletiveToAbsCords(teamText_start, teamTextImage)
         main.paste(teamTextImage, pasteCords, mask=teamTextImage)
         # move the start to the next location
@@ -32,7 +39,7 @@ def RotatedSliderAnimation(speed, teamList, saveImages=False):
 
     ROTATION = 45
     SPACING_VERTICAL = 80
-    ALPHA = 0
+    ALPHA = 150
 
     main = Image.new('RGB', (1920,1080), color='white')
     rawRGB = Image.open(f"{TEAMTEXT_DIR}\\temp.png", "r")
@@ -45,28 +52,30 @@ def RotatedSliderAnimation(speed, teamList, saveImages=False):
         if item[:3] == (0, 0, 0):
             newImage.append((0, 0, 0, 0))
         else:
-            newImage.append(item)
+            newImage.append((item[0], item[1], item[2], ALPHA))
     teamTextImage.putdata(newImage)
 
     currPos = [0, 0] 
-    #xFlip = True
-    cPos_x1 = 0
-    cPos_x2 = 0
-    while currPos[1] <= CANVAS_SIZE[1]*2:
-        #if xFlip:
-        #    oldCurrPos = currPos
-        #    currPos[0] -= (sqrt(teamTextImage.size[0]**2 + teamTextImage.size[1]**2))//2
+    xFlip = True
+
+    while currPos[1] <= CANVAS_SIZE[1]*3:
+        if xFlip:
+            hypot = int((sqrt(teamTextImage.size[0]**2 + teamTextImage.size[1]**2))/2)
+            print(hypot)
+            currPos[0] = hypot
+            xFlip = not xFlip
+        else:
+            currPos[0] = 0
         main, teamTextImage = util_drawLine(currPos, main, teamTextImage)
         currPos[1] += SPACING_VERTICAL
         print(currPos)
 
     print(f"Saved as {os.getcwd()}\\TeamTextOut.png")
-    main.save(os.getcwd() + "\\TeamTextOut.png")
+    main.save(os.getcwd() + "\\TeamTextOut2.png")
     
 
 if __name__ == "__main__":
     print(f"{bcolors.FAIL}Please run main.py for full functionality.{bcolors.ENDC}{bcolors.WARNING}{bcolors.ENDC}")
-
     exit()
 
 
